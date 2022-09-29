8c05c9409bee86cea964ee6d7f4c136d3ec888aa
ilhamsg7 <sinatrio20@gmail.com>
2022-09-29 23:27:14 +0700

    Perbaikan Feature yang belum sesuai

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
+    colors: {
+      theme: "#3fc35f",
+    },
     extend: {
       fontFamily: {
         nunito: ["Nunito", "sans"],
         poppins: ["Poppins", "sans"],
-      }
+      },
     },
   },
-  plugins: [
-        require('flowbite/plugin')
-  ]
-}
+  plugins: [require("flowbite/plugin")],
+};
